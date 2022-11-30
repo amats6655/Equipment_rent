3744e74027cfb33b076efbe1faac228b4a658485
amats6655 <amats6655@gmail.com>
2022-12-01 00:01:04 +0300

    Подготовка основы для авторизации пользователей

---
+++ Auth/Model/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Model
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<User> User { get; set; }
+
+        public ApplicationContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=85.175.4.135,49172\\SQLEXPRESS;DataBase=equipment_rent_auth;User ID=mrkotik;Password=ciscO15=!");
+        }
+    }
+}
